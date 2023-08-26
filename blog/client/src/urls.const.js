e75406adaf950cf9212fc08ecdaa15450300528c
Jay Tailor <tailorj64@gmail.com>
2023-08-26 11:24:24 +0530

    React app to use configured domains

--- blog/client/src/urls.const.js
+++ blog/client/src/urls.const.js
@@ -1,3 +1,3 @@
-export const POSTS_SERVICE_HOST="http://localhost:4000";
-export const COMMENTS_SERVICE_HOST="http://localhost:4001";
-export const QUERY_SERVICE_HOST="http://localhost:4002";
+export const POSTS_SERVICE_HOST="http://posts.com";
+export const COMMENTS_SERVICE_HOST="http://posts.com";
+export const QUERY_SERVICE_HOST="http://posts.com";
