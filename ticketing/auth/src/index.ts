3cbcdabc379773d910937f24b17edcc5cdb3e9d3
Jay Tailor <tailorj64@gmail.com>
2023-08-28 13:04:54 +0530

    Add ingress service

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -4,7 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hello!');
+});
 
 app.listen(3000, () => {
     console.log('Auth service listening on port', 3000);
