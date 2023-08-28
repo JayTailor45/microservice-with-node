94711d547f37e70753ed5af068539f0feafa6889
Jay Tailor <tailorj64@gmail.com>
2023-08-28 16:07:23 +0530

    Handle errors with consistent format

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,8 +1,33 @@
 import { NextFunction, Request, Response } from "express";
+import { RequestValidationError } from "../errors/request-validation.error";
+import { DatabaseConnectionError } from "../errors/database-connection.error";
 
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  if (err instanceof RequestValidationError) {
+    const formattedError = err.errors.map((error) => {
+      if (error.type === "field") {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedError });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({
+      errors: [{ message: err.reason }],
+    });
+  }
+
+  res.status(400).send({
+    errors: [
+      {
+        message: "Something went wrong!",
+      },
+    ],
+  });
+};
