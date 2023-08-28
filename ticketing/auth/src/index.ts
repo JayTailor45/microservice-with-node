5dbf4338d7db09faf32fd2ba6d73bfef59df0608
Jay Tailor <tailorj64@gmail.com>
2023-08-28 13:19:17 +0530

    route refactor

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 
 import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/sign-in";
+import { signOutRouter } from "./routes/sign-out";
+import { signUpRouter } from "./routes/sign-up";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
 
 app.listen(3000, () => {
   console.log("Auth service listening on port", 3000);
