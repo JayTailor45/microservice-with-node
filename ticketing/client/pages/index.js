65a0a94e0d412d981a13721b7064bb0d624c93e5
Jay Tailor <tailorj64@gmail.com>
2023-09-01 14:13:17 +0530

    Add reusable function to make server/client aware api calls

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,44 +1,17 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
-const LandingPage = (props) => {
-  console.log("In the component", props);
-  return <h1>Landing Page</h1>;
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-export async function getServerSideProps() {
-  if (typeof window === "undefined") {
-    // we are on server
-    // request should be made with http://ingress-nginx.ingress-nginx...
-    console.log("On Server");
-
-    try {
-      const response = await axios.get(
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-        {
-          headers: { Host: "ticketing.dev" }, // <- Require to provide Host
-        }
-      );
-
-      const { data } = response;
-      return { props: { ...data } };
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    // we are on browser
-    // request can be made with a base url of ''
-    console.log("On Browser");
-
-    try {
-      const response = await axios.get("/api/users/currentuser");
-      const { data } = response;
-      return { props: { ...data } };
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  return { props: {} };
+export async function getServerSideProps(context) {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return { props: { ...data } };
 }
 
 export default LandingPage;
