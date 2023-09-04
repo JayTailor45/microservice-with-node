e4783c1f602bc9c2449d5a83097879eccfc0b5ee
Jay Tailor <tailorj64@gmail.com>
2023-09-04 16:54:06 +0530

    Handle get tickets route

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -6,6 +6,7 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@tjticket/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.get("*", async () => {
   throw new NotFoundError();
