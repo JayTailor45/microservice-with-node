29c180df73d515ed2162cc84d2181c8346d15293
Jay Tailor <tailorj64@gmail.com>
2023-09-04 14:29:21 +0530

    Init tickets project

---
+++ ticketing/tickets/src/index.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import mongoose from "mongoose";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@tjticket/common";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.get("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  // check if env variables are defined
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY is not found");
+  }
+
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Auth app connected to mongodb");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Ticket service listening on port", 3000);
+  });
+};
+
+start();
