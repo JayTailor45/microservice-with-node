c2014312b9dac33907379ef65fd6f18b7b25b06c
Jay Tailor <tailorj64@gmail.com>
2023-09-04 11:42:26 +0530

    Handle initial data loading from root app

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,7 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import buildClient from "../api/build-client";
+import App from "next/app";
 
-const wrapper = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header! {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default wrapper;
+AppComponent.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  return { ...data, ...appProps };
+};
+
+export default AppComponent;
