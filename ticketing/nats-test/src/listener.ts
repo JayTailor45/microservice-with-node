a9fcdece49689cb791732d58c68b6dfc41402d68
Jay Tailor <tailorj64@gmail.com>
2023-09-05 13:21:12 +0530

    Parse receieved data

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
 
 const stan = nats.connect("ticketing", "123", {
   url: "http://localhost:4222",
@@ -9,7 +9,17 @@ stan.on("connect", () => {
 
   const subscription = stan.subscribe("ticket:created");
 
-  subscription.on("message", (msg) => {
-    console.log("Msg received");
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(
+        `Received event #${msg.getSequence()}, with data: ${JSON.stringify(
+          JSON.parse(data),
+          null,
+          2
+        )}`
+      );
+    }
   });
 });
