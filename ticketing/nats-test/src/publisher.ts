84b10d654c68d65cfbb4f58bcc90773a4f6b647a
Jay Tailor <tailorj64@gmail.com>
2023-09-05 12:46:09 +0530

    Publisher emit events

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -6,4 +6,16 @@ const stan = nats.connect("ticketing", "abc", {
 
 stan.on("connect", () => {
   console.log("Publisher connected to nats");
+
+  const data = {
+    id: "123",
+    title: "conf",
+    price: 20,
+  };
+
+  const jsonString = JSON.stringify(data);
+
+  stan.publish("ticket:created", jsonString, () => {
+    console.log("ticket:created event published");
+  });
 });
