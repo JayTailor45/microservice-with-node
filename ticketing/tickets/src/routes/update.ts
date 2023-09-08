087174428af46b07bea22fdd6c2d4a1b400464c9
Jay Tailor <tailorj64@gmail.com>
2023-09-08 11:18:12 +0530

    Commit ticket updated event

--- ticketing/tickets/src/routes/update.ts
+++ ticketing/tickets/src/routes/update.ts
@@ -8,6 +8,8 @@ import {
 } from "@tjticket/common";
 import { Ticket } from "../models/ticket";
 import { Types as MongooseTypes } from "mongoose";
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated.publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -40,6 +42,13 @@ router.put(
     });
 
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
