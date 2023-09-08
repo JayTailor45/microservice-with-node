a3f57a1514715130553c4f75968d73b106c524eb
Jay Tailor <tailorj64@gmail.com>
2023-09-08 14:05:29 +0530

    Move nats config to env

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -43,9 +43,22 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URL is not found");
   }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL is not found");
+  }
+  if (!process.env.NATS_CLUSTER) {
+    throw new Error("NATS_CLUSTER is not found");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID is not found");
+  }
 
   try {
-    await natsWrapper.connect("ticketing", "test", "http://nats-srv:4222");
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER,
+      process.env.NATS_CLIENT_ID, // <-- Client id will be pod id/name
+      process.env.NATS_URL
+    );
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed!");
       process.exit();
