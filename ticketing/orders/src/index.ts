1d0d5e8c806dc2fba57370ca62ffda530177a642
Jay Tailor <tailorj64@gmail.com>
2023-09-08 17:35:10 +0530

    Orders service initial setup

---
+++ ticketing/orders/src/index.ts
@@ -0,0 +1,71 @@
+import express from "express";
+import mongoose from "mongoose";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError, currentUser } from "@tjticket/common";
+import { natsWrapper } from "./nats-wrapper";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUser);
+
+app.get("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  // check if env variables are defined
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY is not found");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URL is not found");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL is not found");
+  }
+  if (!process.env.NATS_CLUSTER) {
+    throw new Error("NATS_CLUSTER is not found");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID is not found");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER,
+      process.env.NATS_CLIENT_ID, // <-- Client id will be pod id/name
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Ticket app connected to mongodb");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Ticket service listening on port", 3000);
+  });
+};
+
+start();
