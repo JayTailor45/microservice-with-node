1177a11ac9c11a48ae46f5eafaa24f5da411b6e3
Jay Tailor <tailorj64@gmail.com>
2023-09-09 14:18:57 +0530

    Add ticket and orders model

---
+++ ticketing/orders/models/order.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@tjticket/common";
+import { TicketDoc } from "./ticket";
+
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.CREATED,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ticket",
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order();
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("order");
+
+export { Order };
