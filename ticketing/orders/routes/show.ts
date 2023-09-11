410aa5cb6022cbfe79cf5b5f4ed520379784fe3a
Jay Tailor <tailorj64@gmail.com>
2023-09-11 12:57:55 +0530

    Get order by id

--- ticketing/orders/routes/show.ts
+++ ticketing/orders/routes/show.ts
@@ -1,9 +1,31 @@
+import {
+  NotFoundError,
+  UnAuthorizedError,
+  requireAuth,
+} from "@tjticket/common";
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new UnAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
