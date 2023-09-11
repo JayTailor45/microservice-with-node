f4f8fbafe6403757be2af432571153516d073d2a
Jay Tailor <tailorj64@gmail.com>
2023-09-11 13:07:33 +0530

    Cancel order

--- ticketing/orders/routes/delete.ts
+++ ticketing/orders/routes/delete.ts
@@ -1,9 +1,35 @@
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
+import {
+  NotFoundError,
+  OrderStatus,
+  UnAuthorizedError,
+  requireAuth,
+} from "@tjticket/common";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new UnAuthorizedError();
+    }
+
+    order.status = OrderStatus.CANCELLED;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
