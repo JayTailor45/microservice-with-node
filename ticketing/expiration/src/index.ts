e6ed77655ac62de79253db1836b29ceb3f118523
Jay Tailor <tailorj64@gmail.com>
2023-09-12 15:43:47 +0530

    Expiration: Initial app setup

---
+++ ticketing/expiration/src/index.ts
@@ -0,0 +1,33 @@
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  // check if env variables are defined
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL is not found");
+  }
+  if (!process.env.NATS_CLUSTER) {
+    throw new Error("NATS_CLUSTER is not found");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID is not found");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER,
+      process.env.NATS_CLIENT_ID, // <-- Client id will be pod id/name
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (error) {
+    console.error(error);
+  }
+
+};
+
+start();
