6aa7357afe97049948430cf5c2da590a0fc0351b
Jay Tailor <tailorj64@gmail.com>
2023-09-13 13:13:39 +0530

    Payments: validate payment request

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -1,6 +1,13 @@
-import { requireAuth } from "@tjticket/common";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  UnAuthorizedError,
+  requireAuth,
+} from "@tjticket/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -8,7 +15,23 @@ router.post(
   "/api/payments",
   requireAuth,
   [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new UnAuthorizedError();
+    }
+
+    if (order.status === OrderStatus.CANCELLED) {
+      throw new BadRequestError("Cannot play for cancelled order");
+    }
+
     res.send({ success: true });
   }
 );
